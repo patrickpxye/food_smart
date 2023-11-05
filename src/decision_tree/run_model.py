40592d50da02b901d683216a864c7fed18e56a16
Patrick Peixuan Ye <patrickypx@gmail.com>
2023-11-05 00:26:25 -0700

    decision tree

---
+++ src/decision_tree/run_model.py
@@ -0,0 +1,57 @@
+import models
+import torch
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from dataset_builder import ImageDataset
+from torch.utils.data import DataLoader
+
+# initialize the computation device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+#intialize the model
+feature_model = models.feature_model(pretrained=False, requires_grad=False).to(device)
+xgb_model = models.xgb_classifier()
+# load the model checkpoint
+checkpoint = torch.load('../outputs/model.pth')
+# load model weights state_dict
+feature_model.load_state_dict(checkpoint['model_state_dict'])
+feature_model.eval()
+
+# prepare the test dataset and dataloader
+test_data = ImageDataset("../../input/ingredients_classifier/test_images.txt",
+                       "../../input/ingredients_classifier/test_labels.txt",
+                       "../../input/ingredients_classifier/recipes.txt",
+                       "../../input/ingredients_classifier/ingredients.txt",
+                       False,
+                       True)
+test_loader = DataLoader(
+    test_data,
+    batch_size=1,
+    shuffle=False
+)
+
+for counter, data in enumerate(test_loader):
+    image, target = data['image'].to(device), data['label']
+    # get all the index positions where value == 1
+    target_indices = [i for i in range(len(target[0])) if target[0][i] == 1]
+    # get the predictions by passing the image through the model
+    features = feature_model(image).detach().cpu().numpy()
+    # Predict using XGBoost model
+    xgb_predictions = xgb_model.predict(features)
+    sorted_indices = np.argsort(xgb_predictions[0])[::-1]  # reverse the array to get highest values first
+    best = sorted_indices[:5]  # we assume you want the top 5 predictions
+    string_predicted = ''
+    string_actual = ''
+    for i in range(len(best)):
+        string_predicted += f"{test_data.index_to_ingredient[best[i].item()]}    "
+    for i in range(len(target_indices)):
+        string_actual += f"{test_data.index_to_ingredient[target_indices[i]]}    "
+    image = image.squeeze(0)
+    image = image.detach().cpu().numpy()
+    image = np.transpose(image, (1, 2, 0))
+    plt.imshow(image)
+    plt.axis('off')
+    plt.title(f"PREDICTED: {string_predicted}\nACTUAL: {string_actual}")
+    plt.savefig(f"../outputs/inference_{counter}.jpg")
+    #plt.show()
