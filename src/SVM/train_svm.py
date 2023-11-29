9a3e5bdbd5597d7b02edb37576bb5d21d6016cd6
Shoupei Li <shoupeili@meta.com>
2023-11-28 16:43:36 -0800

    revert svm change

--- src/SVM/train_svm.py
+++ src/SVM/train_svm.py
@@ -1,12 +1,14 @@
+from feature_extractor.feature_extractor import FeatureExtractor
 import torch
 import joblib
-import numpy as np
 from tqdm import tqdm
 from torch.utils.data import DataLoader
+from sklearn.multiclass import OneVsRestClassifier
 from dataset.dataset_builder import ImageDataset
+import numpy as np
 from sklearn.svm import LinearSVC
-from sklearn.multioutput import MultiOutputClassifier
-from feature_extractor.feature_extractor import FeatureExtractor
+from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
 
 # initialize the computation device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -57,8 +59,35 @@ for i, data in tqdm(enumerate(train_loader), total=int(len(train_dataset) / trai
 # Train SVM model
 features = np.array(features)
 labels = np.array(labels)
-clf = LinearSVC(penalty='l1', C=1, dual=False)
-multi_target_svm = MultiOutputClassifier(clf, n_jobs=-1)
-multi_target_svm.fit(features, labels)
+clf = OneVsRestClassifier(LinearSVC(penalty='l1', C=1, dual=False))
+clf.fit(features, labels)
 
-joblib.dump(multi_target_svm, '../../outputs/svm_model.pkl')
+'''
+parameters = {
+    #'vect__ngram_range': [(1, 3), (1, 4)],
+    #'vect__max_df': [1, 0.8],
+#     'vect__min_df': [1, 0.8, 0],
+    #'vect__use_idf': [True],
+    'estimator__penalty': ['l1'],
+    'estimator__C': [1, 10, 100],
+    'estimator__dual': [False],
+}
+
+gs_clf = GridSearchCV(clf, parameters, cv=5, n_jobs=-1)
+gs_clf.fit(features, labels)
+
+valid_features = []
+valid_labels = []
+for i, data in tqdm(enumerate(valid_loader), total=int(len(valid_dataset) / valid_loader.batch_size)):
+    valid_data, target = data['image'].to(device), data['label'].to(device)
+    outputs = extractor(valid_data)
+    valid_features.extend(outputs.detach().cpu().numpy())
+    valid_labels.extend(target.detach().cpu().numpy())
+
+Y_predicted = gs_clf.predict(valid_features)
+print(metrics.classification_report(valid_labels, Y_predicted))
+print(gs_clf.best_params_)
+print(gs_clf.best_score_)
+'''
+# Save the model
+joblib.dump(clf, '../../outputs/svm_model.pkl')
